1bbbb16dc6c01602ecccacc197854acd4ded1012
Jeffrey Noh <jnoh12388@gmail.com>
2013-04-04 22:14:58 -0700

    dependent vidoes destroy

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,7 +1,7 @@
 class Conversation < ActiveRecord::Base
   attr_accessible :creator, :invites, :name
 
-  has_many :videos, order: "videos.created_at DESC"
+  has_many :videos, order: "videos.created_at DESC", :dependent => :destroy
   has_many :memberships
   has_many :members, through: :memberships, source: :user, class_name: "User"
   has_many :invites
