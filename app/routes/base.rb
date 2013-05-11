0ac0043043b1e40ed628be02606aedf174e0a51d
Jeffrey Noh <jnoh12388@gmail.com>
2013-05-10 19:02:16 -0700

    enterprise push

--- app/routes/base.rb
+++ app/routes/base.rb
@@ -23,7 +23,7 @@ module HollerbackApp
     end
 
     configure :production do
-      ::APNS.pem = File.join(app_root, 'config', 'apns', 'apns_prod.pem')
+      ::APNS.pem = File.join(app_root, 'config', 'apns', 'apns_enterprise_prod.pem')
       ::APNS.host = 'gateway.push.apple.com'
     end
   end
