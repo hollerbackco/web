d23defba7d76dc5f59121b05691f2fde464ce7fa
Jeffrey Noh <jnoh12388@gmail.com>
2013-05-17 19:15:34 -0700

    fix honeybadger

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -2,6 +2,8 @@ Honeybadger.configure do |config|
   config.api_key = ENV['HONEYBADGER_API_KEY']
 end
 
-if Sinatra::Base.production? or Sinatra::Base.settings.environment == :staging
-  use Honeybadger::Rack
+module HollerbackApp
+  class BaseApp < ::Sinatra::Base
+    use Honeybadger::Rack
+  end
 end
