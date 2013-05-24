00617006a71921409f6688a77d6f28e185ee05e3
Jeffrey Noh <jnoh12388@gmail.com>
2013-05-24 15:21:27 -0700

    added devices

---
+++ app/models/device.rb
@@ -0,0 +1,8 @@
+class Device < ActiveRecord::Base
+  attr_accessible :platform, :platform_version, :token
+
+  validates :platform, presence: true, inclusion: {in: %w(ios android)}
+  validates :token,    presence: true
+
+  belongs_to :user
+end
