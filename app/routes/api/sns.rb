c3ce6188ec2a4e926282ec3a4ad0f6810918f520
Jeffrey Noh <jnoh12388@gmail.com>
2013-06-02 22:31:42 -0700

    recieve raw json data

--- app/routes/api/sns.rb
+++ app/routes/api/sns.rb
@@ -2,10 +2,10 @@
 module HollerbackApp
   class ApiApp < BaseApp
     post '/sns/et' do
-      p params
-      if params.key? "Message"
-        jobId = params["Message"]["jobId"]
-        if params["Message"]["state"] == "COMPLETED"
+      p obj = JSON.parse request.body.read
+      if obj.key? "Message"
+        jobId = obj["Message"]["jobId"]
+        if obj["Message"]["state"] == "COMPLETED"
           StreamJob.find_by_job_id(jobId).complete!
         end
       end
