0071334f679e6355103d133bdde086a367caa2a4
Jeffrey Noh <jnoh12388@gmail.com>
2013-06-02 22:24:26 -0700

    fix sns route

--- app/routes/api/sns.rb
+++ app/routes/api/sns.rb
@@ -3,9 +3,11 @@ module HollerbackApp
   class ApiApp < BaseApp
     post '/sns/et' do
       p params
-      jobId = params["Message"]["jobId"]
-      if params["Message"]["state"] == "COMPLETED"
-        StreamJob.find_by_job_id(jobId).complete!
+      if params.key? "Message"
+        jobId = params["Message"]["jobId"]
+        if params["Message"]["state"] == "COMPLETED"
+          StreamJob.find_by_job_id(jobId).complete!
+        end
       end
     end
   end
