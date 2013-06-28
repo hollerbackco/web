592a103e084f108420f53e517edde5997d3d05a2
Jeffrey Noh <jnoh12388@gmail.com>
2013-06-28 00:36:13 -0700

    get rid of session stuff

--- app/routes/api/main.rb
+++ app/routes/api/main.rb
@@ -105,11 +105,6 @@ module HollerbackApp
       if params["updated_at"]
         updated_at = Time.parse params["updated_at"]
         scope = scope.where("conversations.updated_at > ?", updated_at)
-      elsif session["updated_at"]
-        scope = scope.where("conversations.updated_at > ?", session["updated_at"])
-        session["updated_at"] = Time.now
-      else
-        session["updated_at"] = Time.now
       end
 
       conversations = scope.map do |conversation|
