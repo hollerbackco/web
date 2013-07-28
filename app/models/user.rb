615e0b45d9a70ba4fe1781c061a239f75f09a622
Jeffrey Noh <jnoh12388@gmail.com>
2013-07-27 18:54:50 -0700

    normalized phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -139,7 +139,7 @@ class User < ActiveRecord::Base
 
   def as_json(options={})
     #TODO: uncomment when we add this to the signup flow
-    options = options.merge(:only => [:id, :phone_normalized, :username, :name, :created_at])
+    options = options.merge(:only => [:id, :phone, :phone_normalized, :username, :name, :created_at])
     super(options)
   end
 
