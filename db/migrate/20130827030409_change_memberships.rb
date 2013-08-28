1ab3292899336970beeea2ec601cf17452b64357
Jeffrey Noh <jnoh12388@gmail.com>
2013-08-28 02:21:36 -0700

    most recent thumb url

--- db/migrate/20130827030409_change_memberships.rb
+++ db/migrate/20130827030409_change_memberships.rb
@@ -2,23 +2,24 @@ class ChangeMemberships < ActiveRecord::Migration
   def up
     change_table :memberships do |t|
       t.string :name
-      t.string :thumb_url
+      t.string :most_recent_thumb_url
     end
     update_memberships
   end
 
   def down
     remove_column :memberships, :name
-    remove_column :memberships, :thumb_url
+    remove_column :memberships, :most_recent_thumb_url
   end
 
   def update_memberships
     Membership.all.each do |m|
       next if m.messages.empty?
-      next if message = m.messages.first and message.thumb_url.blank?
+      message = m.messages.first
+      next if message.thumb_url.blank?
 
       p m.name
-      m.thumb_url = message.thumb_url
+      m.most_recent_thumb_url = message.thumb_url
       m.last_message_at = message.sent_at
       m.save
     end
