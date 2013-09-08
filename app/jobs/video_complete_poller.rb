9729caae9be65b87d349aa369d1ceba4b077cd78
Jeffrey Noh <jnoh12388@gmail.com>
2013-09-07 18:43:16 -0700

    add quick cache check before delivering video

--- app/jobs/video_complete_poller.rb
+++ app/jobs/video_complete_poller.rb
@@ -10,16 +10,20 @@ class VideoCompletePoller
       SQSLogger.logger.info "Message body: #{message.body}"
       data = JSON.parse(message.body)
 
-      if video = Video.find(data["video_id"])
+      delivered = HollerbackApp::BaseApp.settings.cache.get("d/#{video.id}") || false
+
+      if video = Video.find(data["video_id"]) and !delivered
         video.update_attributes(filename: data["output"], in_progress: false)
         membership = Membership.where(conversation_id: video.conversation_id, user_id: video.user_id).first
         if membership.present?
           publisher = ContentPublisher.new(membership)
           publisher.publish(video)
         end
+        SQSLogger.logger.info " -- Finished updating #{video.id}"
+        HollerbackApp::BaseApp.settings.cache.set("d/#{video.id}", 1)
+      else
+        SQSLogger.logger.info " -- Already delivered #{video.id}"
       end
-
-      SQSLogger.logger.info " -- Finshed updating #{video.id}"
     end
   end
 end
