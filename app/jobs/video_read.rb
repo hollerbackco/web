f0c4671d96f537d78a02ba6d6d4a666f04d4ff65
Jeffrey Noh <jnoh12388@gmail.com>
2013-09-13 15:44:30 -0700

    Send a mqtt push

--- app/jobs/video_read.rb
+++ app/jobs/video_read.rb
@@ -23,6 +23,7 @@ class VideoRead
 
     def notify_mqtt(message, person)
       MQTT::Client.connect(remote_host: '23.23.249.106', username: "UXiXTS1wiaZ7", password: "G4tkwWMOXa8V") do |c|
+        p "send a mqtt push"
         data = [message.to_sync, message.membership.to_sync]
         c.publish("user/#{person.id}/sync", xtea.encrypt(data.to_json), false, 1)
       end
