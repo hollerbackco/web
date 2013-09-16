c1f26cae6a63531a9b215a914d157dd7e1c9a023
Jeffrey Noh <jnoh12388@gmail.com>
2013-09-16 15:34:22 -0700

    syntax error

--- app/jobs/video_read.rb
+++ app/jobs/video_read.rb
@@ -30,7 +30,7 @@ class VideoRead
       MQTT::Client.connect(remote_host: '23.23.249.106', username: "UXiXTS1wiaZ7", password: "G4tkwWMOXa8V") do |c|
         p "send a mqtt push"
         data = messages.map(&:to_sync)
-        data = << messages.first.membership.to_sync
+        data << messages.first.membership.to_sync
         c.publish("user/#{person.id}/sync", xtea.encrypt(data.to_json), false, 1)
       end
     end
