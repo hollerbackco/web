5ad44d1b107f3a4605308da6d2680a9abd2f0e38
Jeffrey Noh <jnoh12388@gmail.com>
2013-09-25 02:49:35 -0700

    add db pool as env config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,6 +17,8 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    config['pool'] = ENV['DB_POOL'] || 5
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
