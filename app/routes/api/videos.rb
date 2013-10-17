8c34b058d1ec7be14f74797e0aa7b33e7b0110f7
Jeffrey Noh <jnoh12388@gmail.com>
2013-10-17 12:29:08 -0700

    mark as read using watched_ids

--- app/routes/api/videos.rb
+++ app/routes/api/videos.rb
@@ -75,9 +75,14 @@ module HollerbackApp
           watched_at = Time.parse(params[:watched_at])
           messages = messages.before(watched_at)
         end
-        if params[:watched_ids]
-          messages = messages.where(:video_guid => params[:watched_ids])
+        if messages.any?
+          VideoRead.perform_async(messages.map(&:id), current_user.id)
+          unread_count = messages.count
         end
+      end
+
+      if params[:watched_ids]
+        messages = messages.where(:video_guid => params[:watched_ids])
         if messages.any?
           VideoRead.perform_async(messages.map(&:id), current_user.id)
           unread_count = messages.count
