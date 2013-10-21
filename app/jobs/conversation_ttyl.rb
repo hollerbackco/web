6fea40228a3463f65c7396b87ade5d9e63baf595
Jeffrey Noh <jnoh12388@gmail.com>
2013-10-21 14:22:07 -0700

    ttyl copy

--- app/jobs/conversation_ttyl.rb
+++ app/jobs/conversation_ttyl.rb
@@ -10,7 +10,7 @@ class ConversationTtyl
 
     membership.others.each do |other|
       p "notify push to #{other.username}"
-      notify_push(membership.user.username, other)
+      notify_push(membership.user.also_known_as(for: other), other)
     end
   end
 
@@ -30,7 +30,7 @@ class ConversationTtyl
       sender_name: sender_name
     }
 
-    text = "#{sender_name} said ttyl"
+    text = "#{sender_name}: ttyl"
 
     person.devices.ios.each do |device|
       APNS.send_notification(device.token, {
