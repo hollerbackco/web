57a05aa3dd24929383aca80c3a47bbafb5f88b58
Jeffrey Noh <jnoh12388@gmail.com>
2013-10-24 14:05:09 -0700

    update bundle to include houston

--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -2,14 +2,15 @@ module HollerbackApp
   class BaseApp < Sinatra::Base
     configure :development, :staging, :test do
       ::GCMS = GCM.new ENV["GCM_KEY"]
-      ::APNS.pem = File.join(app_root, 'config', 'apns', 'apns_enterprise_dev.pem')
+      pemfile = File.join(app_root, 'config', 'apns', 'apns_enterprise_dev.pem')
+      Hollerback::Push.configure(pemfile, false)
     end
 
     configure :production do
       require 'newrelic_rpm'
       ::GCMS = GCM.new ENV["GCM_KEY"]
-      ::APNS.pem = File.join(app_root, 'config', 'apns', 'apns_enterprise_prod.pem')
-      ::APNS.host = 'gateway.push.apple.com'
+      pemfile = File.join(app_root, 'config', 'apns', 'apns_enterprise_prod.pem')
+      Hollerback::Push.configure(pemfile, true)
     end
   end
 end
--- lib/hollerback/push.rb
+++ lib/hollerback/push.rb
@@ -2,7 +2,7 @@ module Hollerback
   class Push
     class << self
       def configure(pemfile, is_production=false)
-        @client = self.client(pemfile, production)
+        @client = self.client(pemfile, is_production)
       end
 
       def send(token, options={})
@@ -15,14 +15,13 @@ module Hollerback
         notification.badge = badge if badge
         notification.sound = sound if sound
 
-        self.client.push(notification)
+        @client.push(notification)
       end
 
       def client(pemfile, is_production)
-        return @client if @client
         client = is_production ? Houston::Client.production : Houston::Client.development
         client.certificate = File.read(pemfile)
-        @client = client
+        client
       end
     end
   end
