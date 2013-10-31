1053dc201f349e5bd655d4abdc8a4e1407bad692
Jeffrey Noh <jnoh12388@gmail.com>
2013-10-31 12:03:58 -0700

    ios fix invite redirects

--- app/routes/web/main.rb
+++ app/routes/web/main.rb
@@ -70,31 +70,41 @@ module HollerbackApp
       end
     end
 
+    get '/invite' do
+      #TODO: check if android or ios
+      app_link = AppLink.where(slug: party, segment: "ios").first_or_create
+      app_link.increment!(:downloads_count)
+
+      url = "http://appstore.com/hollerback"
+      redirect url
+    end
+
     get '/beta/test/:branch' do
       url = URI.escape("https://s3.amazonaws.com/hb-distro/HollerbackApp-#{params[:branch]}.plist")
       redirect "itms-services://?action=download-manifest&url=#{url}"
     end
 
     get '/beta/:party' do
-      party = params[:party]
-
-      if Sinatra::Base.production? and ! params.key? :test
-        Hollerback::SMS.send_message "+13033595357", "[ios] #{party} visited the beta page"
-      end
-
-      app_link = AppLink.where(slug: party, segment: "ios").first_or_create
-
       if params[:party] == "teamhollerback"
-        app_link.increment!(:downloads_count)
         url = URI.escape("https://s3.amazonaws.com/hb-distro/HollerbackApp-staging.plist")
-        redirect "itms-services://?action=download-manifest&url=#{url}"
+        url = "itms-services://?action=download-manifest&url=#{url}"
       elsif app_link.usable?
+        app_link = AppLink.where(slug: party, segment: "ios").first_or_create
+        if Sinatra::Base.production? and ! params.key? :test
+          Hollerback::SMS.send_message "+13033595357", "[ios] #{params[:party]} was sent to the appstore"
+        end
         app_link.increment!(:downloads_count)
-        url = URI.escape("https://s3.amazonaws.com/hb-distro/HollerbackApp-master.plist")
-        redirect "itms-services://?action=download-manifest&url=#{url}"
+
+        #to enterprise build
+        #url = URI.escape("https://s3.amazonaws.com/hb-distro/HollerbackApp-master.plist")
+        #redirect "itms-services://?action=download-manifest&url=#{url}"
+        #
+
+        url = "http://appstore.com/hollerback"
       else
-        redirect "/"
+        url = "/"
       end
+      redirect url
     end
 
     ['/fly', '/fly/:from_name'].each do |path|
