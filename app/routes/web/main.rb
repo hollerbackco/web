3c86f9909195c58faebbf8038f5ef8dd6345567e
Jeffrey Noh <jnoh12388@gmail.com>
2013-11-01 17:11:43 -0700

    reset password

--- app/routes/web/main.rb
+++ app/routes/web/main.rb
@@ -1,13 +1,19 @@
 module HollerbackApp
   class WebApp < BaseApp
-    helpers do
-      def omniauth
-        @omniauth ||= request.env["omniauth.auth"]
-      end
-    end
-
     get '/' do
       haml :index, layout: false
+    end
+
+    get '/waitlist' do
+      haml :waitlist
+    end
+
+    get '/terms' do
+      haml :terms
+    end
+
+    get '/privacy' do
+      haml :privacy
     end
 
     #TODO deprecate, replaced by v/:token
@@ -36,114 +42,8 @@ module HollerbackApp
       haml :video
     end
 
-    get '/waitlist' do
-      haml :waitlist
-    end
-
-    get '/terms' do
-      haml :terms
-    end
-
-    get '/privacy' do
-      haml :privacy
-    end
-
-    get '/android/:party' do
-      party = params[:party]
-
-      if Sinatra::Base.production? and ! params.key? :test
-        Hollerback::SMS.send_message "+13033595357", "[android] #{party} visited the beta page"
-      end
-
-      app_link = AppLink.where(slug: party, segment: "android").first_or_create
-
-      if params[:party] == "teamhollerback"
-        app_link.increment!(:downloads_count)
-        url = URI.escape("https://s3.amazonaws.com/hollerback-app-dev/distro/HollerbackAndroid-Stage.apk")
-        redirect url
-      elsif app_link.usable?
-        app_link.increment!(:downloads_count)
-        url = URI.escape("https://s3.amazonaws.com/hollerback-app-dev/distro/HollerbackAndroid.apk")
-        redirect url
-      else
-        redirect "/"
-      end
-    end
-
-    get '/invite' do
-      #TODO: check if android or ios
-      app_link = AppLink.where(slug: "invite", segment: "ios").first_or_create
-      app_link.increment!(:downloads_count)
-
-      url = "http://appstore.com/hollerback"
-      redirect url
-    end
-
-    get '/beta/test/:branch' do
-      url = URI.escape("https://s3.amazonaws.com/hb-distro/HollerbackApp-#{params[:branch]}.plist")
-      redirect "itms-services://?action=download-manifest&url=#{url}"
-    end
-
-    get '/beta/:party' do
-      app_link = AppLink.where(slug: params[:party], segment: "ios").first_or_create
-      if params[:party] == "teamhollerback"
-        url = URI.escape("https://s3.amazonaws.com/hb-distro/HollerbackApp-staging.plist")
-        url = "itms-services://?action=download-manifest&url=#{url}"
-      elsif app_link.usable?
-        if Sinatra::Base.production? and ! params.key? :test
-          Hollerback::SMS.send_message "+13033595357", "[ios] #{params[:party]} was sent to the appstore"
-        end
-        app_link.increment!(:downloads_count)
-
-        #to enterprise build
-        #url = URI.escape("https://s3.amazonaws.com/hb-distro/HollerbackApp-master.plist")
-        #redirect "itms-services://?action=download-manifest&url=#{url}"
-        #
-
-        url = "http://appstore.com/hollerback"
-      else
-        url = "/"
-      end
-      redirect url
-    end
-
-    ['/fly', '/fly/:from_name'].each do |path|
-      get path do
-        #@signup_test = ab_test("signup_waitlist", 'twitter', 'email')
-        if params.key? "from_name"
-          session[:from] = params[:from_name]
-        end
-
-        haml :glass, layout: :pledge
-      end
-    end
-
-    get '/auth/:provider/callback' do
-      pledger = Pledger.where(username: omniauth[:info][:nickname]).first_or_create do |p|
-        p.name = omniauth[:info][:name]
-        p.username = omniauth[:info][:nickname]
-        p.auth_token = omniauth[:credentials][:token]
-        p.auth_secret = omniauth[:credentials][:secret]
-        p.meta =  omniauth[:extra][:raw_info].as_json
-
-        if session.key? :from
-          if from = Pledger.where(username: session[:from]).first
-            p.parent_id = from.id
-          end
-        end
-      end
-      redirect to("/pledge/#{pledger.username}")
-    end
-
     get "/thanks" do
       finished("signup_waitlist")
-      haml :entries, layout: :pledge
-    end
-
-    get "/pledge/:username" do
-      finished("signup_waitlist")
-      @pledger = Pledger.where(username: params[:username]).first
-
       haml :entries, layout: :pledge
     end
 
