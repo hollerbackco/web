45f947c47df02b1732c4228c7fdcb028517205f2
Jeffrey Noh <jnoh12388@gmail.com>
2013-11-08 02:51:56 -0800

    change room for bmo

--- lib/hollerback/bmo.rb
+++ lib/hollerback/bmo.rb
@@ -3,7 +3,7 @@ module Hollerback
     def self.say(msg)
       if Sinatra::Base.production? 
         uri = URI('http://still-depths-4143.herokuapp.com/hubot/say')
-        res = Net::HTTP.post_form(uri, 'room' => '60453_hollerback@conf.hipchat.com', 'message' => msg)
+        res = Net::HTTP.post_form(uri, 'room' => 'general', 'message' => msg)
       end
     end
   end
