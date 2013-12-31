26d71766c72d6cad274826a6aee078ce0c87a670
Jeffrey Noh <jnoh12388@gmail.com>
2013-12-31 11:26:41 -0800

    add guid to hash

--- app/models/message.rb
+++ app/models/message.rb
@@ -130,7 +130,7 @@ class Message < ActiveRecord::Base
 
   def as_json(opts={})
     options = {}
-    options = options.merge(:methods => [:url, :thumb_url, :conversation_id, :user, :is_deleted, :subtitle])
+    options = options.merge(:methods => [:guid, :url, :thumb_url, :conversation_id, :user, :is_deleted, :subtitle])
     options = options.merge(:only => [:created_at, :sender_name, :sent_at, :needs_reply])
     options = options.merge(opts)
     super(options).merge({isRead: !unseen?, id: guid})
