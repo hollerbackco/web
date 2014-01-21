a18fb70ff4223d1c38fdc6945393ea5facd0f295
Jeffrey Noh <jnoh12388@gmail.com>
2014-01-20 17:12:29 -0800

    friend object

--- app/routes/api/friends.rb
+++ app/routes/api/friends.rb
@@ -1,11 +1,24 @@
 module HollerbackApp
   class ApiApp < BaseApp
+
+    helpers do
+      def friend_objects_for_user(friends, user)
+        friends.map do |friend|
+          {
+            id: friend.id,
+            username: friend.username,
+            name: friend.also_known_as(for: user)
+          }
+        end
+      end
+    end
+
     get '/me/friends' do
       friends = current_user.friends
 
       data = {
         recent_friends: [],
-        friends: friends
+        friends: friend_objects_for_user(friends, current_user)
       }
 
       success_json data: data.as_json
@@ -21,7 +34,8 @@ module HollerbackApp
         for friend in friends
           current_user.friendships.where(friend_id: friend.id).first_or_create
         end
-        success_json data: friends.as_json
+
+        success_json data: friend_objects_for_user(friends, current_user)
       else
         success_json data: []
       end
