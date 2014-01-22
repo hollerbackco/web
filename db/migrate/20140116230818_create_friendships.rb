f550769d780558c53444b6e84d11469a94c0b5b2
Jeffrey Noh <jnoh12388@gmail.com>
2014-01-21 16:17:19 -0800

    fix migration

--- db/migrate/20140116230818_create_friendships.rb
+++ db/migrate/20140116230818_create_friendships.rb
@@ -1,12 +1,12 @@
 class CreateFriendships < ActiveRecord::Migration
   def up
-    #create_table :friendships do |t|
-      #t.integer :user_id
-      #t.integer :friend_id
-    #end
+    create_table :friendships do |t|
+      t.integer :user_id
+      t.integer :friend_id
+    end
 
-    #add_index :friendships, :user_id
-    #add_index :friendships, :friend_id
+    add_index :friendships, :user_id
+    add_index :friendships, :friend_id
   end
 
   def down
