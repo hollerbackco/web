b593c4ad34eaa22191c2799240c15d693ee4ac3a
stabib <sajjad@hollerback.co>
2014-03-03 15:55:39 -0800

    removed dead code

--- app/routes/api/main.rb
+++ app/routes/api/main.rb
@@ -75,13 +75,6 @@ module HollerbackApp
       success_json data: current_user.as_json.merge(conversations: current_user.conversations)
     end
 
-    post '/me/invites' do
-      unless ensure_params(:invites)
-        return error_json 400, msg: "missing required param: invites"
-      end
-      success_json data: nil
-    end
-
     post '/me' do
       device = Device.find_by_access_token(params[:access_token])
       device.token = params["device_token"]
