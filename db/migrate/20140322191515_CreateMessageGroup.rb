dedaf6e2e1e7dbbbb28462007a0d08f7c37ee152
stabib <sajjad@hollerback.co>
2014-03-22 15:23:52 -0400

    added the create message group migration

---
+++ db/migrate/20140322191515_CreateMessageGroup.rb
@@ -0,0 +1,16 @@
+class Createmessagegroup < ActiveRecord::Migration
+  def up
+    create_table :message_groups do |t|
+      t.integer     :membership_id
+      t.string      :group_type
+      t.hstore      :group_info
+      t.timestamps
+    end
+
+    add_index :message_groups, :membership_id
+    add_index :message_groups, :group_info
+  end
+
+  def down
+  end
+end
