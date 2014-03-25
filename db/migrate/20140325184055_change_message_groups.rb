a191409e0aa635ba4bad997abc02a8308efaa747
stabib <sajjad@hollerback.co>
2014-03-25 16:56:16 -0400

    optimized a bit

--- db/migrate/20140325184055_change_message_groups.rb
+++ db/migrate/20140325184055_change_message_groups.rb
@@ -11,6 +11,40 @@ class ChangeMessageGroups < ActiveRecord::Migration
     update_db
   end
 
+  def create_groups
+    messages = Messages.all.order(:sent_at)
+
+    #for each user get the memberships
+    memberships = Memberships.all
+
+      memberships.each do |membership|
+
+        messages = membership.messages.order(:sent_at)
+
+        message_group = nil #a message group
+
+        messages.each do |message|
+
+          next if message.sent_at.blank?
+          #case when a group has already been created
+          if (!message_group.nil? && (message.sent_at - Time.parse(message_group.group_info["end_time"])) <= 60 && (message.sender_id.to_s == message_group.group_info["sender_id"]))
+            message_group.group_info["end_time"] = message.sent_at
+            message_group.messages << message
+            message.save
+            message_group.save
+            next
+          else #create a new message group if we can't add it to the previous one
+            group_info = {"start_time" => message.sent_at, "end_time" => message.sent_at, "sender_id" => message.sender_id}
+            message_group = MessageGroup.create(:group_info => group_info)
+            message_group.messages << message
+            membership.message_groups << message_group
+            message_group.save
+            message.save
+          end
+        end #messages.do
+      end #membership.do
+  end
+
   def update_db
     #get each user
     users = User.all
