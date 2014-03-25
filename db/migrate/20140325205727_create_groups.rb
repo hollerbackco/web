e2cbff9ec6c9dfc2995906f1e1e4a9d747dc0363
stabib <sajjad@hollerback.co>
2014-03-25 18:17:31 -0400

    update the group creation

---
+++ db/migrate/20140325205727_create_groups.rb
@@ -0,0 +1,48 @@
+class CreateGroups < ActiveRecord::Migration
+  def change
+
+    create_table :message_groups do |t|
+      t.integer     :membership_id
+      t.string      :group_type
+      t.hstore      :group_info
+      t.timestamps
+    end
+
+    add_index :message_groups, :membership_id
+    add_index :message_groups, :group_info
+
+    create_groups
+  end
+
+  def create_groups
+    #for each user get the memberships
+    memberships = Membership.all
+
+    memberships.each do |membership|
+
+      messages = membership.messages.order(:sent_at)
+
+      message_group = nil #a message group
+
+      messages.each do |message|
+
+        next if message.sent_at.blank?
+        #case when a group has already been created
+        if (!message_group.nil? && (message.sent_at - Time.parse(message_group.group_info["end_time"])) <= 60 && (message.sender_id.to_s == message_group.group_info["sender_id"]))
+          message_group.group_info["end_time"] = message.sent_at
+          message_group.messages << message
+          message.save
+          message_group.save
+          next
+        else #create a new message group if we can't add it to the previous one
+          group_info = {"start_time" => message.sent_at, "end_time" => message.sent_at, "sender_id" => message.sender_id}
+          message_group = MessageGroup.create(:group_info => group_info)
+          message_group.messages << message
+          membership.message_groups << message_group
+          message_group.save
+          message.save
+        end
+      end #messages.do
+    end #membership.do
+  end
+end #class
