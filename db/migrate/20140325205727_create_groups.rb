55d652f4f167edd91188b0bb62e059bfdd603ab7
stabib <sajjad@hollerback.co>
2014-03-26 17:09:21 -0400

    damn@

--- db/migrate/20140325205727_create_groups.rb
+++ db/migrate/20140325205727_create_groups.rb
@@ -1,16 +1,5 @@
 class CreateGroups < ActiveRecord::Migration
   def change
-
-    create_table :message_groups do |t|
-      t.integer     :membership_id
-      t.string      :group_type
-      t.hstore      :group_info
-      t.timestamps
-    end
-
-    add_index :message_groups, :membership_id
-    add_index :message_groups, :group_info
-
     create_groups
   end
 
