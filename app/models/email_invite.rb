f93d417e825c417dd4cc8a398f91e46cb1e40007
stabib <sajjad@hollerback.co>
2014-03-30 17:56:01 -0400

    remove uniqueness restriction on email invites

--- app/models/email_invite.rb
+++ app/models/email_invite.rb
@@ -2,7 +2,7 @@ class EmailInvite < ActiveRecord::Base
   belongs_to :inviter, class_name: "User"
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
 
   before_save {self.email = email.downcase}
 end
