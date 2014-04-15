e068ecd1ccdd427895936e394a3a6fcd20cfe2af
stabib <sajjad@hollerback.co>
2014-04-15 16:47:37 -0400

    updated intercom code

--- app/services/intercom_publisher.rb
+++ app/services/intercom_publisher.rb
@@ -25,15 +25,9 @@ class IntercomPublisher
   end
 
   def update_user(user, user_agent)
-<<<<<<< HEAD
     payload = get_user_payload(user, user_agent)
     Intercom::User.create(payload)
     impression(user, user_agent)
-=======
-    impression(user, user_agent)
-    payload = get_user_payload(user, user_agent)
-    Intercom::User.create(payload)
->>>>>>> bb8b8e3... intercom support
   end
 
   def impression(user, user_agent)
